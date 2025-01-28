5b8ae6f6a5f2c5db5816a32b537560c41031b801
MasonJWelch <162480500+MasonJWelch@users.noreply.github.com>
2025-01-28 21:10:03 +0000

    commit

---
+++ hello.py
@@ -0,0 +1,30 @@
+print("Hello World")
+# This is a single line comment
+""" Data Types:
+int - Integer (whole number)
+float - Floating-point Number (number w/ a decimal)
+str - String (any text, in quotes)
+chr - Character (single keyboard character, in single quotes)
+bool - Boolea (Either True or False, only)
+"""
+
+""" Arithmetic Operators:
++ Add
+- Subtract
+* Multiply
+/ Divide
+// Floor Divide (Divide and round down)
+% Mod (Modulus/Remainder from Int division)
+** Power
+"""
+
+""" Assignment Operators:
+= Value assignment (x = y)
++= Add to (x += y is shorthand for x = x + )
+-= Subtract from
+*=
+/=
+//=
+%=
+**=
+"""
